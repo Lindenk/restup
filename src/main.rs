172c2d2bc38cde65883cfb0da0e344f0d4b48ff0
Linden Krouse <linden@krouse.tech>
2025-05-20 04:28:15 +0000

    Added logging

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use axum::{
 };
 use clap::Parser;
 use log::info;
+use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
 use tokio::process::Command;
 use tower_http::services::ServeDir;
 
@@ -18,22 +19,41 @@ mod error;
 
 #[tokio::main]
 async fn main() {
+  // init logging
+  if let Err(e) = TermLogger::init(
+    log::LevelFilter::Info,
+    Config::default(),
+    TerminalMode::Mixed,
+    ColorChoice::Auto,
+  ) {
+    println!("Failed to initialize logging, {}", e);
+  }
+
+  // init cli args
   let args = config::Args::parse();
 
+  // set up our routes
   let app = Router::new()
     .route("/", get(|| async { "OK" }))
     .nest_service("/sensors/raw", ServeDir::new(args.sensor_dir.clone()))
     .route("/commands/raw/{*command}", post(run_command))
     .with_state(args.clone());
 
+  // bind our network interface
   let listener = tokio::net::TcpListener::bind((args.interface, args.port))
     .await
     .expect("Failed to bind to interface");
+
+  // start the server
+  if let Ok(addr) = listener.local_addr() {
+    info!("Serving on {:?}", addr);
+  };
   axum::serve(listener, app)
     .await
     .expect("Failed to serve endpoints");
 }
 
+/// Runs the executable at the path given relative to the base `command_dir`
 async fn run_command(
   State(config): State<config::Args>,
   Path(command): Path<PathBuf>,
